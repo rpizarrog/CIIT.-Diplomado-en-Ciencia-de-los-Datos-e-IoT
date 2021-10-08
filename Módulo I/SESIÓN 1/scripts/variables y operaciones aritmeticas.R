04041e81b780d8c0315311b13ebdbb86b9794e7c
rpizarrog <rpizarro@itdurango.edu.mx>
2021-10-08 17:54:00 -0500

    Add files via upload

---
+++ Módulo I/SESIÓN 1/scripts/variables y operaciones aritmeticas.R
@@ -0,0 +1,19 @@
+# Declarar variables y hacer 
+# operaciones aritméticas
+
+num1 = 30
+num2 = 50
+
+suma = num1 + num2
+resta = num1 - num2
+divis = num1 / num2
+prod = num1 * num2
+potencia = num1 ^ 2
+
+print(suma)
+cat(resta)
+paste(divis)
+prod
+potencia
+
+
